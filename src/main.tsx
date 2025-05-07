1ac1d38eb95b89fe1c09036f4a2b2bf4082d8fc4
Brae <francesco.frediani@icloud.com>
2025-05-08 01:26:24 +0200

    Add @vercel/analytics dependency and integrate Analytics component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './i18n'; // Importa la configurazione di i18next
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
+
